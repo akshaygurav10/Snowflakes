246499efd851050e08dcc34dee30cb72fe552036
Akshay Gurav <akshaygurav10@gmail.com>
2026-03-18 03:50:13 +0000

    Final commit

--- models/window_function.sql
+++ models/window_function.sql
@@ -3,12 +3,10 @@ with windows_function as(
     select 
     hire_date,
     department_id,
-    count(employee_id),
-
-    row_number() OVER (PARTITION BY HIRE_DATE ORDER BY count(employee_id) ) AS ROWNUM
-
+     count(employee_id)
     from {{ source('AG_SOURCE_DEMO', 'EMPLOYEES') }}
     group by hire_date,department_id
+    QUALIFY row_number() OVER (PARTITION BY HIRE_DATE ORDER BY count(employee_id) ) = 1
     order by hire_date
 )
 SELECT * FROM windows_function
